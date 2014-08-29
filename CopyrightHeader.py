fe30a4d2b02fa0caf08599123b9bd646b5906575
Robyn Allsman <robyn@LSST.org>
2014-08-29 16:55:55 -0500

    Update Copyrights for owenership and website.

--- CopyrightHeader.py
+++ CopyrightHeader.py
@@ -1,6 +1,7 @@
 # 
 # LSST Data Management System
-# Copyright 2008, 2009, 2010, 2011 LSST Corporation.
+#
+# Copyright 2008-2014  AURA/LSST.
 # 
 # This product includes software developed by the
 # LSST Project (http://www.lsst.org/).
@@ -17,5 +18,5 @@
 # 
 # You should have received a copy of the LSST License Statement and 
 # the GNU General Public License along with this program.  If not, 
-# see <http://www.lsstcorp.org/LegalNotices/>.
+# see <https://www.lsstcorp.org/LegalNotices/>.
 #
