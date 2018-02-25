f787ae2a75643b8271f53e53a12c595fd2bbaa49
Jonathan Sick <jsick@lsst.org>
2018-02-24 23:27:39 -0700

    Add scons builder for cookiecutter proj examples
    - templatekit.builder.cookiecutter_project_builder is a scons builder
      for cookiecutter projects. It renders examples only (using defaults in
      cookiecutter.json). Users will use the cookiecutter CLI directly to
      generate new projects for themselves.
    
    - Implement SConscript file for the example_project/ cookiecutter
      project.

---
+++ project_templates/example_project/example/demo_file.rst
@@ -0,0 +1,7 @@
+##############
+Templated file
+##############
+
+This is an example of a file with a templated name.
+
+Copyright 2018 Association of Universities for Research in Astronomy
--- templatekit/builder.py
+++ templatekit/builder.py
@@ -1,8 +1,11 @@
 """Scons builders.
 """
 
-__all__ = ('file_template_builder',)
+__all__ = ('file_template_builder', 'cookiecutter_project_builder')
 
+import os
+
+from cookiecutter.main import cookiecutter
 from SCons.Script import Builder
 
 from .filerender import render_and_write_file_template
@@ -30,4 +33,34 @@ file_template_builder = Builder(action=build_file_template,
                                 suffix='',
                                 src_suffix='.jinja')
 """Scons builder for rendering a single-file template examples.
+"""
+
+
+def build_project_template(target, source, env):
+    """Scons builder action for rendering a cookiecutter project template.
+
+    Parameters
+    ----------
+    target : `list` of `Scons.Script.Node`
+        A list of Node objects corresponding to examples to be built.
+    source : `list` of `Scons.Script.Node`
+        A list of Node objects containing only the ``cookiecutter.json`` file
+        node.
+    env : `Scons.Script.Environment`
+        The construction environment used for building the target.
+    """
+    # target_path = str(target[0])
+    cookiecutter_json_source = str(source[0])
+
+    template_dir = os.path.dirname(cookiecutter_json_source)
+
+    cookiecutter(
+        template_dir,
+        output_dir=template_dir,
+        overwrite_if_exists=True,
+        no_input=True)
+
+
+cookiecutter_project_builder = Builder(action=build_project_template)
+"""Scons builder for rendering a cookiecutter project template.
 """
