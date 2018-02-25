8d9cd75fea2ad1dad9e1223a3f6c3e8f37efca36
Jonathan Sick <jsick@lsst.org>
2018-02-25 13:06:21 -0700

    Add emit_cookiecutter_sources Scons emitter
    This lets the template designer just point the scons builder to a
    cookiecutter project's cookiecutter.json file. The emitter function then
    modifies the scons sources list to add all files in the cookiecutter
    template itself. In doing so, we can successfully use the Scons's
    dependency caching mechanism.

--- templatekit/builder.py
+++ templatekit/builder.py
@@ -1,4 +1,16 @@
-"""Scons builders.
+"""Scons builders for regenerating examples given template defaults.
+
+This module provides two builders:
+
+- ``build_file_template`` for single-file templates in the
+  ``file_templates`` directory.
+
+- ``build_project_template`` for cookiecutter (project) templates in the
+  ``project_templates`` directory.
+
+Again, Scons is only used by the templates repository to regenerate examples
+given the template defaults. Users will use cookiecutter directly to generate
+new projects from a template.
 """
 
 __all__ = ('file_template_builder', 'cookiecutter_project_builder')
@@ -6,6 +18,7 @@ __all__ = ('file_template_builder', 'cookiecutter_project_builder')
 import os
 
 from cookiecutter.main import cookiecutter
+from cookiecutter.find import find_template
 from SCons.Script import Builder
 
 from .filerender import render_and_write_file_template
@@ -61,6 +74,28 @@ def build_project_template(target, source, env):
         no_input=True)
 
 
-cookiecutter_project_builder = Builder(action=build_project_template)
+def emit_cookiecutter_sources(target, source, env):
+    """Emit the full list of sources for a Cookiecutter project, based on
+    the root ``cookiecutter.json`` source.
+
+    This is a **Scons emitter** that is used with the
+    `cookiecutter_project_builder` to establish a project template's full
+    source list.
+    """
+    # Get the template directory (i.e., "{{ cookiecutter.project_name }}/")
+    template_dir = find_template('.')
+    # Get all the template files and add them to the sources
+    for (_base_path, _dir_names, _file_names) in os.walk(template_dir):
+        source.extend([os.path.join(_base_path, file_name)
+                       for file_name in _file_names])
+    return target, source
+
+
+cookiecutter_project_builder = Builder(action=build_project_template,
+                                       emitter=emit_cookiecutter_sources)
 """Scons builder for rendering a cookiecutter project template.
+
+The action is `build_project_template` and the emitter is
+`emit_cookiecutter_sources`, which sets up the full dependency tree for a
+cookiecutter project.
 """
