5f090221fa2c9149d175797fa749ecff5b8489fa
Jonathan Sick <jsick@lsst.org>
2018-03-26 15:55:26 -0700

    Create example_pythononly
    This stack_package example has `uses_cpp` = False, so it's a good
    example of what a Python-only package looks like. This example takes
    advantage of the post_gen_hook to delete the lib/src/include directories
    only needed for C++ projects.

---
+++ project_templates/stack_package/example_pythononly/doc/conf.py
@@ -0,0 +1,13 @@
+"""Sphinx configuration file for an LSST stack package.
+
+This configuration only affects single-package Sphinx documenation builds.
+"""
+
+from documenteer.sphinxconfig.stackconf import build_package_configs
+import lsst.example.pythononly
+
+
+_g = globals()
+_g.update(build_package_configs(
+    project_name='example_pythononly',
+    version=lsst.example.pythononly.version.__version__))
