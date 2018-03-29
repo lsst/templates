0b9e17c9e255ec7af82232f88c8cb6fc6529f183
Jonathan Sick <jsick@lsst.org>
2018-03-29 09:45:00 -0700

    Add Jinja filters for creating header files.
    - convert_py_namespace_to_cpp_header_def creates "LSST_EXAMPLE_H" from
      "lsst.example".
    
    - convert_py_to_cpp_namespace creates "lsst::example"

--- templatekit/jinjaext.py
+++ templatekit/jinjaext.py
@@ -1,7 +1,9 @@
 """Custom Jinja2 filters and tags.
 """
 
-__all__ = ('convert_py_to_cpp_namespace_code',)
+__all__ = ('convert_py_to_cpp_namespace_code',
+           'convert_py_namespace_to_cpp_header_def',
+           'convert_py_to_cpp_namespace')
 
 from jinja2.ext import Extension
 
@@ -31,12 +33,17 @@ class TemplatekitExtension(Extension):
     **Included filters**
 
     - ``convert_py_to_cpp_namespace_code`` (`convert_py_to_cpp_namespace`)
+    - ``convert_py_namespace_to_cpp_header_def``
+      (`convert_py_namespace_to_cpp_header_def`)
+    - ``convert_py_to_cpp_namespace`` (`convert_py_to_cpp_namespace`)
     """
 
     def __init__(self, environment):
         super().__init__(environment)
 
         environment.filters['convert_py_to_cpp_namespace_code'] = convert_py_to_cpp_namespace_code  # noqa: E501
+        environment.filters['convert_py_namespace_to_cpp_header_def'] = convert_py_namespace_to_cpp_header_def  # noqa: E501
+        environment.filters['convert_py_to_cpp_namespace'] = convert_py_to_cpp_namespace  # noqa: E501
 
 
 def convert_py_to_cpp_namespace_code(python_namespace):
@@ -70,3 +77,37 @@ def convert_py_to_cpp_namespace_code(python_namespace):
     opening = 'namespace ' + ' { '.join(namespace_parts) + ' {\n'
     closing = '}' * len(namespace_parts) + ' // {}'.format(name)
     return '\n'.join((opening, closing))
+
+
+def convert_py_namespace_to_cpp_header_def(python_namespace):
+    """Convert a Python namespace into a C++ header def token.
+
+    Parameters
+    ----------
+    python_namespace : `str`
+        A string describing a Python namespace. For example,
+        ``'lsst.example'``.
+
+    Returns
+    -------
+    cpp_header_def : `str`
+        C++ header def, such as '`'LSST_EXAMPLE_H'``.
+    """
+    return python_namespace.upper().replace('.', '_') + '_H'
+
+
+def convert_py_to_cpp_namespace(python_namespace):
+    """Convert a Python namespace name to a C++ namespace.
+
+    Parameters
+    ----------
+    python_namespace : `str`
+        A string describing a Python namespace. For example,
+        ``'lsst.example'``.
+
+    Returns
+    -------
+    cpp_namespace : `str`
+        A C++ namespace. For example: ``'lsst::example'``.
+    """
+    return python_namespace.replace('.', '::')
--- tests/test_jinjaext.py
+++ tests/test_jinjaext.py
@@ -1,7 +1,10 @@
 """Tests for the templatekit.jinjaext module.
 """
 
-from templatekit.jinjaext import convert_py_to_cpp_namespace_code
+from templatekit.jinjaext import (
+    convert_py_to_cpp_namespace_code,
+    convert_py_namespace_to_cpp_header_def,
+    convert_py_to_cpp_namespace)
 
 
 def test_cpp_namespace_code():
@@ -16,3 +19,19 @@ def test_cpp_namespace_code():
     )
 
     assert expected == convert_py_to_cpp_namespace_code(python_namespace)
+
+
+def test_cpp_header_def():
+    """Test convert_py_namespace_to_cpp_header_def.
+    """
+    python_namespace = 'lsst.example'
+    expected = 'LSST_EXAMPLE_H'
+    assert expected == convert_py_namespace_to_cpp_header_def(python_namespace)
+
+
+def test_cpp_namespace():
+    """Test convert_py_to_cpp_namespace.
+    """
+    python_namespace = 'lsst.example'
+    expected = 'lsst::example'
+    assert expected == convert_py_to_cpp_namespace(python_namespace)
