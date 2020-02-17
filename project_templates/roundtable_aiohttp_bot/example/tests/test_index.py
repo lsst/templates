94df8c1adec3530a4908d5f3573f7c2fef4ff811
Jonathan Sick <jsick@lsst.org>
2020-02-17 14:48:36 -0500

    Add tests for the index routes
    These tests use the pytest-aiohttp plugin to help us set up a client for
    the app and test the JSON response.

---
+++ project_templates/roundtable_aiohttp_bot/example/tests/test_index.py
@@ -0,0 +1,22 @@
+"""Tests for the example.handlers.external.index module and routes.
+"""
+
+from example.app import create_app
+
+
+async def test_get_index(aiohttp_client):
+    """Test GET /app-name/
+    """
+    app = create_app()
+    name = app['safir/config'].name
+    client = await aiohttp_client(app)
+
+    response = await client.get(f"/{name}/")
+    assert response.status == 200
+    data = await response.json()
+    metadata = data['_metadata']
+    assert metadata['name'] == name
+    assert isinstance(metadata['version'], str)
+    assert isinstance(metadata['description'], str)
+    assert isinstance(metadata['repository_url'], str)
+    assert isinstance(metadata['documentation_url'], str)
---
+++ project_templates/roundtable_aiohttp_bot/{{cookiecutter.repo_name}}/tests/test_internal_index.py
@@ -0,0 +1,20 @@
+"""Tests for the {{ cookiecutter.package_name }}.handlers.internal.index module and routes.
+"""
+
+from {{ cookiecutter.package_name }}.app import create_app
+
+
+async def test_get_index(aiohttp_client):
+    """Test GET /
+    """
+    app = create_app()
+    client = await aiohttp_client(app)
+
+    response = await client.get("/")
+    assert response.status == 200
+    data = await response.json()
+    assert data['name'] == app['safir/config'].name
+    assert isinstance(data['version'], str)
+    assert isinstance(data['description'], str)
+    assert isinstance(data['repository_url'], str)
+    assert isinstance(data['documentation_url'], str)
