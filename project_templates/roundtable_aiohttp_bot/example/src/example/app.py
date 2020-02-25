71f9c956a005c9bc9aea8647d8dcba7e4f255ca9
Jonathan Sick <jsick@lsst.org>
2020-02-25 15:55:41 -0500

    Switch to safir.metadata

--- project_templates/roundtable_aiohttp_bot/example/src/example/app.py
+++ project_templates/roundtable_aiohttp_bot/example/src/example/app.py
@@ -3,21 +3,14 @@
 
 __all__ = ["create_app"]
 
-import sys
-from typing import Any, Dict
-
 from aiohttp import web
 from safir.http import init_http_session
 from safir.logging import configure_logging
+from safir.metadata import setup_metadata
 from safir.middleware import bind_logger
 
 from example.config import Configuration
 from example.handlers import init_external_routes, init_internal_routes
-
-if sys.version_info < (3, 8):
-    from importlib_metadata import metadata, PackageNotFoundError
-else:
-    from importlib.metadata import metadata, PackageNotFoundError
 
 
 def create_app() -> web.Application:
@@ -32,7 +25,7 @@ def create_app() -> web.Application:
 
     root_app = web.Application()
     root_app["safir/config"] = config
-    setup_metadata(app=root_app, config=config)
+    setup_metadata(package_name=example, app=root_app)
     setup_middleware(root_app)
     root_app.add_routes(init_internal_routes())
     root_app.cleanup_ctx.append(init_http_session)
@@ -49,24 +42,3 @@ def setup_middleware(app: web.Application) -> None:
     """Add middleware to the application.
     """
     app.middlewares.append(bind_logger)
-
-
-def setup_metadata(*, app: web.Application, config: Configuration) -> None:
-    """Add a metadata object to the application under the ``safir/metadata``
-    key.
-    """
-    try:
-        pkg_metadata = metadata("example")
-    except PackageNotFoundError:
-        pkg_metadata = {}
-
-    meta: Dict[str, Any] = {
-        # Use configured name in case it is dynamically changed.
-        "name": config.name,
-        # Get metadata from the package configuration
-        "version": pkg_metadata.get("Version", "0.0.0"),
-        "description": pkg_metadata.get("Summary", None),
-        "repository_url": "https://github.com/lsst-sqre/example",
-        "documentation_url": pkg_metadata.get("Home-page", None),
-    }
-    app["safir/metadata"] = meta
