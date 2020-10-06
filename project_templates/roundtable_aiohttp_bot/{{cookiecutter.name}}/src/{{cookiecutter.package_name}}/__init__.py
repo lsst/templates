76d28a567d7a08faa895cf4848db544c1ebd8e99
Jonathan Sick <jsick@lsst.org>
2020-10-06 16:10:13 -0400

    Fix __all__ statements
    There were some extra symbols left over in __all__ from a refactoring;
    this cleans those up. Also cleans up an import for making the initial
    isort run cleaner.

--- project_templates/roundtable_aiohttp_bot/{{cookiecutter.name}}/src/{{cookiecutter.package_name}}/__init__.py
+++ project_templates/roundtable_aiohttp_bot/{{cookiecutter.name}}/src/{{cookiecutter.package_name}}/__init__.py
@@ -1,13 +1,13 @@
 """The {{ cookiecutter.package_name }} service."""
 
-__all__ = ["__version__", "metadata"]
+__all__ = ["__version__"]
 
 import sys
 
 if sys.version_info < (3, 8):
-    from importlib_metadata import version, PackageNotFoundError
+    from importlib_metadata import PackageNotFoundError, version
 else:
-    from importlib.metadata import version, PackageNotFoundError
+    from importlib.metadata import PackageNotFoundError, version
 
 
 __version__: str
