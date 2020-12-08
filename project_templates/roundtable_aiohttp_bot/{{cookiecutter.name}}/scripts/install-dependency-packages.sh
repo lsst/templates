095199acd7046180a016c69e14d375772467fa75
Russ Allbery <rra@lsst.org>
2020-12-07 17:10:03 -0800

    Update roundtable_aiohttp_bot template
    Add a new script that installs packages required only in the
    dependencies image and install build-essential by default, since
    this is often required for building Python extensions, particularly
    when upgrading to a new Python version.
    
    Use a test matrix of Python 3.8 and 3.9 by default.  Stop pinning
    the version in tox and use whatever the default Python version is.
    Remove or update a few more references to Python 3.7.
    
    Update pre-commit hook versions to the latest.
    
    Use the @v2 syntax for the GitHub Actions configuration, since all
    the actions move the major version tag with each release.  This
    avoids requiring updates for each minor release.

--- project_templates/roundtable_aiohttp_bot/{{cookiecutter.name}}/scripts/install-dependency-packages.sh
+++ project_templates/roundtable_aiohttp_bot/{{cookiecutter.name}}/scripts/install-dependency-packages.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-# This script updates packages in the base Docker image that's used by both the
-# build and runtime images, and gives us a place to install additional
-# system-level packages with apt-get.
+# This script installs additional packages used by the dependency image but
+# not needed by the runtime image, such as additional packages required to
+# build Python dependencies.
 #
-# Based on the blog post:
-# https://pythonspeed.com/articles/system-packages-docker/
+# Since the base image wipes all the apt caches to clean up the image that
+# will be reused by the runtime image, we unfortunately have to do another
+# apt-get update here, which wastes some time and network.
 
 # Bash "strict mode", to help catch problems and bugs in the shell
 # script. Every bash script you write should include this. See
@@ -23,15 +24,9 @@ export DEBIAN_FRONTEND=noninteractive
 # Update the package listing, so we know what packages exist:
 apt-get update
 
-# Install security updates:
-apt-get -y upgrade
-
 # Install build-essential because sometimes Python dependencies need to build
 # C modules, particularly when upgrading to newer Python versions.
 apt-get -y install --no-install-recommends build-essential
-
-# Example of installing a new package, without unnecessary packages:
-apt-get -y install --no-install-recommends git
 
 # Delete cached files we don't need anymore:
 apt-get clean
