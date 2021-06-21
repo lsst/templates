5c54dfdd54d86c51ffebe27c9db6962815878728
Russ Allbery <rra@lsst.org>
2021-06-21 16:31:30 -0700

    Use importlib.metadata unconditionally
    In the FastAPI project template, use importlib.metadata
    unconditionally since Python 3.8 or later is now required.

--- project_templates/fastapi_safir_app/{{cookiecutter.name}}/src/{{cookiecutter.package_name}}/__init__.py
+++ project_templates/fastapi_safir_app/{{cookiecutter.name}}/src/{{cookiecutter.package_name}}/__init__.py
@@ -2,13 +2,7 @@
 
 __all__ = ["__version__"]
 
-import sys
-
-if sys.version_info < (3, 8):
-    from importlib_metadata import PackageNotFoundError, version
-else:
-    from importlib.metadata import PackageNotFoundError, version
-
+from importlib.metadata import PackageNotFoundError, version
 
 __version__: str
 """The application version string (PEP 440 / SemVer compatible)."""
