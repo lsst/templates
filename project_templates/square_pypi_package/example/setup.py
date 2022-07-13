d8b23999dca4b3f4fd8e9d0d8f65234ce6395131
Jonathan Sick <jsick@lsst.org>
2022-07-13 13:54:07 -0400

    Add docstring to setup.py
    This satisfies pydocstyle and accomplishes the same end.

--- project_templates/square_pypi_package/example/setup.py
+++ project_templates/square_pypi_package/example/setup.py
@@ -1,5 +1,8 @@
-# Although this project follows PEP 517, a setup.py is included for backwards
-# compatibility for older setuptools and editabable pip installs.
+"""Legacy setuptools build front-end (pre PEP 517).
+
+Although this project follows PEP 517, a setup.py is included for backwards
+compatibility for older setuptools and editabable pip installs.
+"""
 
 from setuptools import setup
 
