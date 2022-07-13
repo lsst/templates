0951c8f51e53d639d1b19da3493cbaec1b7fe31b
Jonathan Sick <jsick@lsst.org>
2022-07-13 16:28:06 -0400

    Add a docstring for tests module
    Originally this was to make this file pass pydocstyle, but generally it
    labels the intent of this boilerplate file.

--- project_templates/square_pypi_package/example/tests/__init__.py
+++ project_templates/square_pypi_package/example/tests/__init__.py
@@ -1,1 +1,4 @@
-# This file enables test modules to import support code inside tests/
+"""Tests and test support modules."""
+
+# This __init__.py file enables test modules to import support code
+# inside tests/
