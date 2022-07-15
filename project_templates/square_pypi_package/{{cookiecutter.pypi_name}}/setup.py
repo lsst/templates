155baf1d6bc8dba0bd4c7986172612021e49e30e
Jonathan Sick <jsick@lsst.org>
2022-07-15 16:59:51 -0400

    Move project metadata to pyproject.toml
    And drop both setup.cfg and setup.py.

--- project_templates/square_pypi_package/{{cookiecutter.pypi_name}}/setup.py
+++
@@ -1,9 +0,0 @@
-"""Legacy setuptools build front-end (pre PEP 517).
-
-Although this project follows PEP 517, a setup.py is included for backwards
-compatibility for older setuptools and editabable pip installs.
-"""
-
-from setuptools import setup
-
-setup(use_scm_version=True)
