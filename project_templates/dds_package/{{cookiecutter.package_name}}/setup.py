9c524abc267dbd7758655f7e53203d3783f3a64e
Eric Coughlin <ecoughlin@lsst.org>
2024-02-29 09:32:05 -0300

    Add DDS package template

---
+++ project_templates/dds_package/{{cookiecutter.package_name}}/setup.py
@@ -0,0 +1,26 @@
+#
+# This file is part of ts_cbp.
+#
+# Developed for the Rubin Observatory Telescope and Site System.
+# This product includes software developed by the LSST Project
+# (https://www.lsst.org).
+# See the COPYRIGHT file at the top-level directory of this distribution
+# for details of code ownership.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+import setuptools_scm
+from setuptools import setup
+
+setup(version=setuptools_scm.get_version())
