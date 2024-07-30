38fea8e91e0502b92f68eee171b2d7769d58972f
Russ Allbery <rra@lsst.org>
2024-07-30 08:07:54 -0700

    Remove use_helm option to FastAPI template
    We now always use Helm for Kubernetes configuration for our
    applications. Remove the use_helm configuration option for the
    FastAPI Safir app project template and the manifests directory that
    was included if the use_helm option was set to False.

--- project_templates/fastapi_safir_app/hooks/post_gen_project.py
+++ project_templates/fastapi_safir_app/hooks/post_gen_project.py
@@ -3,20 +3,10 @@
 This script runs from the root directory of the created project itself. In
 addition, cookiecutter interpolates Jinja2 templates to insert any necessary
 variables.
-
-This is used to remove the ``manifests`` directory if the project is using
-Helm rather than Kustomize, and remove empty files that aren't relevant in
-some configuration cases.
 """
 
 import os
 import shutil
 
 # These variables are interpolated by cookiecutter before this hook is run
-uses_helm = True if '{{ cookiecutter.uses_helm }}' == 'True' else False
 github_org = '{{ cookiecutter.github_org }}'
-
-# Remove the Kustomize configuration if the package will be using Helm.
-if uses_helm:
-    print(f"(post-gen hook) Removing manifests directory")
-    shutil.rmtree("manifests", ignore_errors=True)
