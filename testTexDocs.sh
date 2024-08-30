49e50795ad0e5d08b0417fa50c6778bd251f5ff0
William O'Mullane <womullan@lsst.org>
2024-08-30 17:05:04 -0400

    ASCOM latex as a paper style
    This reverts commit bcd57aba050c126c00cbb02bf6b2b87b56960a63.

--- testTexDocs.sh
+++ testTexDocs.sh
@@ -5,6 +5,7 @@ DOCS="/latex_lsstdoc/EXAMPLE-0/
 /technote_aastex/testn-000/ 
 /technote_adasstex/testn-000/ 
 /technote_spietex/testn-000/ 
+/technote_ascomtex/testn-000/ 
 /technote_latex/testn-000/ 
 /test_report/TESTTR-0/ 
 "
